ca5f30a2ae91b4a09fbd673ad5250b90084c4060
Ben Hogan <benjamin.hogan1@kaluza.com>
2022-09-08 10:12:39 +0100

    feat: add featureE (#26)

--- src/features/index.ts
+++ src/features/index.ts
@@ -2,3 +2,4 @@ export { featureA } from './featureA';
 export { featureB } from './featureB';
 export { featureC } from './featureC';
 export { featureD } from './featureD';
+export { featureE } from './featureE';
