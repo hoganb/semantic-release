1db7514de1ad344ac6812e323f3a7d81b93ea205
Ben Hogan <benjamin.hogan1@kaluza.com>
2022-09-08 10:31:59 +0100

    feat: add featureF

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 console.log('Hello from @hoganb/semantic-release');
-export { featureA, featureB, featureC, featureD, featureE } from './features';
+export { featureA, featureB, featureC, featureD, featureE, featureF } from './features';
