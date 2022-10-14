4b7920fcd580a3e6e7c25b2768bd97df6564ed62
Ben Hogan <benjamin.hogan1@kaluza.com>
2022-10-14 11:17:16 +0100

    ci: json

--- src/manual.ts
+++ src/manual.ts
@@ -4,7 +4,7 @@ const input = process.env.JSON_INPUT;
 console.log(input);
 if (input) {
     const json = JSON.parse(input);
-    console.log(JSON.stringify(json, null, 4));
+    console.log(json);
 }
 
 console.log('END: Manual JSON input');
